ddb51e162705154aa26dcb4c6ac38f8505a4433c
root <root@corey-laptop.hersheykiss>
2018-07-14 15:29:47 -0700

    boilerplate setup

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const title = "Minimal React Webpack Babel Setup";
+
+ReactDOM.render(<div>{title}</div>, document.getElementById("app"));
+
+module.hot.accept();
